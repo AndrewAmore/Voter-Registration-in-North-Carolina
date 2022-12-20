4e30a76aea599cd6354bc0f177d251c84a474871
AndrewAmore <awamore@gmail.com>
2022-12-19 23:55:52 -0500

    edits to main report

--- data/Census_API/api_scipt.R
+++ data/Census_API/api_scipt.R
@@ -13,15 +13,36 @@ df = df[-1,]
 
 ## cast relevant fields
 df$POP = as.numeric(df$POP)
-df$AGEGROUP = as.numeric(df$AGEGROUP)
+# df$AGEGROUP = as.numeric(df$AGEGROUP)
 
 ## field code conversion 
-## 18-25, 26-40, 41-65, 65+
-age_groups = c()
 # (https://www.census.gov/data/developers/data-sets/popest-popproj/popest/popest-vars.html)
+
+### AgeGroup (there is not an exact match between sources)
+## 18-24, 25-44, 45-64, 65+
+age_groups = c("23", "24", "25", "26")
+df = df %>% filter(AGEGROUP %in% age_groups)
+
+### Race 
+## WhiteAlone, BlackAlone, AmericanIndianOrAlaskaNativeAlone, 
+## AsianAlone, NativeHawaiianOrOtherPacificIslanderAlone, 
+## TwoOrMoreRaces, SomeOtherRaceAlone
+race_ = c("1", "2", "3", "4", "5", "6")
 df %>% mutate(
-  
+  RACE = case_when(
+    RACE == "1" ~ "WhiteAlone",
+    RACE == "2" ~ "BlackAlone",
+    RACE == "3" ~ "AmericanIndianOrAlaskaNativeAlone"
+    
+  )
 )
+
+
+
+### clean-up county name
+df$NAME = gsub(" County, North Carolina", "", df$NAME)
+
+
 
 
 
--- data/README.md
+++ data/README.md
@@ -1,10 +1,9 @@
 ## Data
 Describes the data elements used in the analysis. The original assignment provided
 processed flat files from the 2010 Census and 2016 voter registration records from
-a North Carolina State Agency.
-
-The initial analysis 
-
+a North Carolina State Agency. Population estimates from the 2010 Census were shown
+to be invalid for 2016 election and more recent estimates from the official Census
+API were gathered. 
 
 ### Description of Contents
 - ```./Census_API/```: new logic for processing Census API data for more accurate
